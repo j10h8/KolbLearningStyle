52976d8095af89312d5aebba3c26769254660f79
j10h8 <john.hallbeck@outlook.com>
2023-08-18 12:52:12 +0200

    Add initial MainDb migration

---
+++ kolb-learning-style/Migrations/20230818104823_Inital.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace kolb_learning_style.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inital : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Questions",
+                columns: table => new
+                {
+                    QuestionId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Alternative_1 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Alternative_2 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Alternative_3 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Alternative_4 = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Questions", x => x.QuestionId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Results",
+                columns: table => new
+                {
+                    ResultId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Tester = table.Column<int>(type: "int", nullable: false),
+                    IdeaGiver = table.Column<int>(type: "int", nullable: false),
+                    Gatherer = table.Column<int>(type: "int", nullable: false),
+                    Explainer = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Results", x => x.ResultId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Questions");
+
+            migrationBuilder.DropTable(
+                name: "Results");
+        }
+    }
+}
