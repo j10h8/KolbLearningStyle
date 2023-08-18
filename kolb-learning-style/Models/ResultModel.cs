1b1b0b1b7f3924075b660cb076d85d80e3029844
David86Nilsson <113332119+David86Nilsson@users.noreply.github.com>
2023-08-18 12:11:15 +0200

    Result & Question

---
+++ kolb-learning-style/Models/ResultModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace kolb_learning_style.Models
+{
+    public class ResultModel
+    {
+        [Key]
+        public int ResultId { get; set; }
+        public int Tester { get; set }
+        public int IdeaGiver { get; set; }
+        public int Gatherer { get; set; }
+        public int Explainer { get; set; }
+    }
+}
