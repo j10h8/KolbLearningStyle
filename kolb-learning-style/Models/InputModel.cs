b25422af50b06099a8d07b8cc00703a6d7118f2c
j10h8 <john.hallbeck@outlook.com>
2023-08-20 21:00:36 +0200

    Add EditForm and InputModel ErrorMessage

--- kolb-learning-style/Models/InputModel.cs
+++ kolb-learning-style/Models/InputModel.cs
@@ -5,20 +5,21 @@ namespace kolb_learning_style.Models
 	public class InputModel
 	{
 		[Required(ErrorMessage = "Du måste ange 1, 2, 3 eller 4 i varje fält.")]
-		[Range(1, 4)]
+		[Range(1, 4, ErrorMessage = "Du måste ange 1, 2, 3 eller 4 i varje fält.")]
 		public int Input11 { get; set; }
 		[Required(ErrorMessage = "Du måste ange 1, 2, 3 eller 4 i varje fält.")]
-		[Range(1, 4)]
+		[Range(1, 4, ErrorMessage = "Du måste ange 1, 2, 3 eller 4 i varje fält.")]
 		public int Input12 { get; set; }
 		[Required(ErrorMessage = "Du måste ange 1, 2, 3 eller 4 i varje fält.")]
-		[Range(1, 4)]
+		[Range(1, 4, ErrorMessage = "Du måste ange 1, 2, 3 eller 4 i varje fält.")]
 		public int Input13 { get; set; }
 		[Required(ErrorMessage = "Du måste ange 1, 2, 3 eller 4 i varje fält.")]
-		[Range(1, 4)]
+		[Range(1, 4, ErrorMessage = "Du måste ange 1, 2, 3 eller 4 i varje fält.")]
 		public int Input14 { get; set; }
 		[Required(ErrorMessage = "Du måste ange 1, 2, 3 eller 4 i varje fält.")]
-		[Range(1, 4)]
+		[Range(1, 4, ErrorMessage = "Du måste ange 1, 2, 3 eller 4 i varje fält.")]
 		public int Input21 { get; set; }
+
 		// And so on for all 36 inputs. 
 	}
 }
